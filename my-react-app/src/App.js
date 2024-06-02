3c8f90a2b3f1fbee74c66daf566dc58132469d20
nban22 <nban22@clc.fitus.edu.vn>
2024-06-02 12:58:02 +0700

    useState()

---
+++ my-react-app/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+const courses = [
+  {
+    id: 1,
+    name: "HTML, CSS"
+  },
+  {
+    id: 2,
+    name: "Javascript"
+  },
+  {
+    id: 3,
+    name: "ReactJS"
+  }
+]
+
+const cssCenterTag = {
+  marginTop: '50px',
+  display: 'flex',
+  alignItems: 'center',
+  // justifyContent: 'center',
+  flexDirection: 'column',
+  height: '100vh'
+}
+
+function App() {
+  const [item, setItem] = useState("");
+  const [listItem, setListItem] = useState(JSON.parse(localStorage.getItem('item')) ?? []);
+  const handleSubmit = () => {
+    if (item.trim() !== "") {
+      console.log(listItem);
+      setListItem(() => {
+        const newList = [...listItem, item];
+        const jsonItem = JSON.stringify(newList);
+        localStorage.setItem('item', jsonItem)
+        return newList;
+      });
+      setItem("");
+    }
+  };
+
+  return (
+    <div style={cssCenterTag}>
+      <div>
+        <input 
+          id='inputtag'
+          value={item}
+          onChange={(e) => {
+            setItem(e.target.value);
+          }}
+          onKeyDown={(e)=>{
+            if (e.key === "Enter") {
+              handleSubmit();
+            }
+          }}
+          />
+        <button
+          onClick={handleSubmit}
+        >Add</button>
+      </div>
+      <ul>
+        {listItem.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
